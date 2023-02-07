1e9cfb7c4e65271ffd08601c6b7d89011bcc38b5
Jishnulal7 <jishnulal9497@gmail.com>
2023-02-07 10:26:31 +0530

    Update

--- basics.dart
+++ basics.dart
@@ -1,53 +1,99 @@
-void main() {
-  // Data types
+// import 'dart:ffi';
+// import 'dart:html';
 
-  // int studentCount = 30;
-  // print(studentCount.isEven);
-  // print(studentCount.isOdd);
-  // print(studentCount.isNegative);
-  // dynamic count = 10.99;
-  // print(count.round());
-  // List<String> students = ['Jishnu', 'Ajay', 'Sageer'];
-  // print(students.last);
-  // print(students.isEmpty);
-  // print(students.isNotEmpty);
-  // print(students.length);
-  // students.add('Sajeee');
-  // students.insert(0, 'LaL');
-  // students.removeAt(1);
-  // print(students);
+// void main() {
+// Data types
 
-  // Map<String, dynamic> student = {'name': 'LaL', 'mark': 45, 'phone': 95445};
-  // student.addAll({'email':'123@gmail.com'});
-  // print(student);
+// int studentCount = 30;
+// print(studentCount.isEven);
+// print(studentCount.isOdd);
+// print(studentCount.isNegative);
+// dynamic count = 10.99;
+// print(count.round());
+// List<String> students = ['Jishnu', 'Ajay', 'Sageer'];
+// print(students.last);
+// print(students.isEmpty);
+// print(students.isNotEmpty);
+// print(students.length);
+// students.add('Sajeee');
+// students.insert(0, 'LaL');
+// students.removeAt(1);
+// print(students);
+
+// Map<String, dynamic> student = {'name': 'LaL', 'mark': 45, 'phone': 95445};
+// student.addAll({'email':'123@gmail.com'});
+// print(student);
 
 // Loopss
 
-  // int num = 1;
-  // while (num <= 10) {
-  //   print(num);
-  //   num++; // num = num + 1;
-  // }
-  // List<String> students = ['Jishnu', 'Ajay', 'Sageer'];
+// int num = 1;
+// while (num <= 10) {
+//   print(num);
+//   num++; // num = num + 1;
+// }
+// List<String> students = ['Jishnu', 'Ajay', 'Sageer'];
 
-  // int count = 0;
-  // while (count < students.length) {
-  //   print(students[count]);
-  //   count++;
-  // }
+// int count = 0;
+// while (count < students.length) {
+//   print(students[count]);
+//   count++;
+// }
 //   for (int i = 0; i < students.length; i++) {
 //     print(students[i]);
 //   }
-  // int userInput = 5;
-  // for (int a = 0; a < userInput; a++) {
-  //   for (int i = 0; i < students.length; i++) {
-  //     print(students[i]);
-  //   }
-  //   print('********');
-  // }
-  // print('end!');
-
+// int userInput = 5;
+// for (int a = 0; a < userInput; a++) {
+//   for (int i = 0; i < students.length; i++) {
+//     print(students[i]);
+//   }
+//   print('********');
+// }
+// print('end!');
 
 //Functionss
 
-}
+// printName();
+
+// String returnVAlue = printName();
+// print(returnVAlue);
+
+// }
+
+// Void printName() {
+//   String name = 'jishnulal';
+//   print(name);
+// }
+
+// String printName() {
+//   String name = 'jishnulal';
+//   return name;
+// }
+
+// void main() {
+//   String name = 'Jishnulal';
+//   printNames(name);
+// }
+
+// String printNames(String name) {
+//   print(name);
+//   return name;
+// }
+// void main() {
+//   String name = 'LaL';
+//   printNames(name);
+// }
+
+// void printNames(String name) {
+//   print(name);
+// }
+
+// void main() {
+//   String name = 'Jishnulal';
+//   String newName = printNames(name);
+//   print(newName);
+// }
+
+// String printNames(String name) {
+//   String newName = name + '......';
+//   return newName;
+// }
