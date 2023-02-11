3c60bfa327bc12c187b59335a1b85ff34e51ba92
Jishnulal7 <jishnulal9497@gmail.com>
2023-02-11 11:22:40 +0530

    Update

---
+++ class-example.dart
@@ -0,0 +1,17 @@
+void main() {
+  var p1 = Person(name: 'LaL', age: 30);
+  print(p1);
+  // print(p1.age);
+  // print(p1.name);
+  var p2 = Person(age: 31, name: 'LaL');
+  // p2.name = 'LaL';
+  print(p2.age) ;
+  // print(p2.name);
+  print(p2);
+}
+
+class Person {
+  String name;
+  int age;
+  Person({ required this.name, required this.age}) {}
+}
